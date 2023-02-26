f7261067a06e9368022f2752064fa6181d2b9903
woiiwod <tobias.lindback@outlook.com>
2023-02-26 14:23:58 +0100

    SE implemented

--- app/include/dt-bindings/zmk/keymap_swedish.h
+++ app/include/dt-bindings/zmk/keymap_swedish.h
@@ -1,23 +1,10 @@
-/* Copyright 2017 Andreas Lindhé
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 #pragma once
 
+//#include <dt-bindings/zmk/keys.h>
 
-// clang-format off
+
 
 /*
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
@@ -33,31 +20,57 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_SECT GRAVE  // §
-#define SE_1    N1     // 1
-#define SE_2    N2     // 2
-#define SE_3    N3     // 3
-#define SE_4    N4     // 4
-#define SE_5    N5     // 5
-#define SE_6    N6     // 6
-#define SE_7    N7     // 7
-#define SE_8    N8     // 8
-#define SE_9    N9     // 9
-#define SE_0    N0     // 0
-#define SE_PLUS MINUS  // +
-#define SE_ACUT EQUAL  // ´ (dead)
+#define SE_SCTR  GRAVE  // §
+#define SE_1     N1     // 1
+#define SE_2     N2     // 2
+#define SE_3     N3     // 3
+#define SE_4     N4     // 4
+#define SE_5     N5     // 5
+#define SE_6     N6     // 6
+#define SE_7     N7     // 7
+#define SE_8     N8     // 8
+#define SE_9     N9     // 9
+#define SE_0     N0     // 0
+#define SE_PLUS  MINUS  // +
+#define SE_ACAC  EQUAL  // ´ (dead)
 // Row 2
-#define SE_ARNG LBKT   // Å
-#define SE_DIAE RBKT   // ¨ (dead)
+#define SE_Q     Q      // Q
+#define SE_W     W      // W
+#define SE_E     E      // E
+#define SE_R     R      // R
+#define SE_T     T      // T
+#define SE_Y     Y      // Y
+#define SE_U     U      // U
+#define SE_I     I      // I
+#define SE_O     O      // O
+#define SE_P     P      // P
+#define SE_AO    LBKT   // Å
+#define SE_DRSS  RBKT   // ¨ (dead)
 // Row 3
-#define SE_ODIA SEMI    // Ö
-#define SE_ADIA SQT     // Ä
-#define SE_QUOT NUHS    // '
+#define SE_A     A      // A
+#define SE_S     S      // S
+#define SE_D     D      // D
+#define SE_F     F      // F
+#define SE_G     G      // G
+#define SE_H     H      // H
+#define SE_J     J      // J
+#define SE_K     K      // K
+#define SE_L     L      // L
+#define SE_OE    SEMI   // Ö
+#define SE_AE    SQT    // Ä
+#define SE_SQT   NUHS   // '
 // Row 4
-#define SE_LABK NUBS    // <
-#define SE_MINS SLASH   // -
+#define SE_Z     Z      // Z
+#define SE_X     X      // X
+#define SE_C     C      // C
+#define SE_V     V      // V
+#define SE_B     B      // B
+#define SE_N     N      // N
+#define SE_M     M      // M
+#define SE_LT    NUBS   // <
+#define SE_MINUS SLASH  // -
 
-/* Shifted symbols
+/* Shifted numbers and symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
  * │ ½ │ ! │ " │ # │ ¤ │ % │ & │ / │ ( │ ) │ = │ ? │ ` │       │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
@@ -71,30 +84,30 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_HALF LS(SE_SECT) // ½
-#define SE_EXLM LS(SE_1)    // !
-#define SE_DQUO LS(SE_2)    // "
-#define SE_HASH LS(SE_3)    // #
-#define SE_CURR LS(SE_4)    // ¤
-#define SE_PERC LS(SE_5)    // %
-#define SE_AMPR LS(SE_6)    // &
-#define SE_SLSH LS(SE_7)    // /
-#define SE_LPRN LS(SE_8)    // (
-#define SE_RPRN LS(SE_9)    // )
-#define SE_EQL  LS(SE_0)    // =
-#define SE_QUES LS(SE_PLUS) // ?
-#define SE_GRV  LS(SE_ACUT) // ` (dead)
+#define SE_HALF  LS(SE_SECT) // ½
+#define SE_EXCL  LS(SE_1)    // !
+#define SE_DQT   LS(SE_2)    // "
+#define SE_HASH  LS(SE_3)    // #
+#define SE_SCAR  LS(SE_4)    // ¤
+#define SE_PRCNT LS(SE_5)    // %
+#define SE_AMPS  LS(SE_6)    // &
+#define SE_SLASH LS(SE_7)    // /
+#define SE_LPAR  LS(SE_8)    // (
+#define SE_RPAR  LS(SE_9)    // )
+#define SE_EQL   LS(SE_0)    // =
+#define SE_QMARK LS(SE_PLUS) // ?
+#define SE_GRAVE LS(SE_ACAC) // ` (dead)
 // Row 2
-#define SE_CIRC LS(SE_DIAE) // ^ (dead)
+#define SE_CARET LS(SE_DIAE) // ^ (dead)
 // Row 3
-#define SE_ASTR LS(SE_QUOT) // *
+#define SE_STAR  LS(SE_QUOT) // *
 // Row 4
-#define SE_RABK LS(SE_LABK) // >
-#define SE_SCLN LS(SE_COMM) // ;
-#define SE_COLN LS(SE_DOT)  // :
-#define SE_UNDS LS(SE_MINS) // _
+#define SE_GT    LS(SE_LT)   // >
+#define SE_SEMI  LS(SE_COMM) // ;
+#define SE_COLON LS(SE_DOT)  // :
+#define SE_UNDER LS(SE_MINS) // _
 
-/* AltGr symbols
+/* RAlt symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
  * │   │   │ @ │ £ │ $ │ € │   │ { │ [ │ ] │ } │ \ │   │       │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
@@ -108,19 +121,19 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_AT   RALT(SE_2)    // @
-#define SE_PND  RALT(SE_3)    // £
-#define SE_DLR  RALT(SE_4)    // $
-#define SE_EURO RALT(SE_5)    // €
-#define SE_LCBR RALT(SE_7)    // {
-#define SE_LBRC RALT(SE_8)    // [
-#define SE_RBRC RALT(SE_9)    // ]
-#define SE_RCBR RALT(SE_0)    // }
-#define SE_BSLS RALT(SE_PLUS) // (backslash)
+#define SE_AT    RALT(SE_2)    // @
+#define SE_PND   RALT(SE_3)    // £
+#define SE_DLLR  RALT(SE_4)    // $
+#define SE_EURO  RALT(SE_5)    // €
+#define SE_LBRC  RALT(SE_7)    // {
+#define SE_LBKT  RALT(SE_8)    // [
+#define SE_RBKT  RALT(SE_9)    // ]
+#define SE_RBRC  RALT(SE_0)    // }
+#define SE_BSLH  RALT(SE_PLUS) // (backslash)
 // Row 2
-#define SE_TILD RALT(SE_DIAE) // ~ (dead)
+#define SE_TILDE RALT(SE_DIAE) // ~ (dead)
 // Row 4
-#define SE_PIPE RALT(SE_LABK) // |
-#define SE_MICR RALT(M)       // µ
+#define SE_PIPE  RALT(SE_LABK)  // |
+#define SE_MICRO RALT(M)        // µ
 
 
